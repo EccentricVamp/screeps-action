48d66c8f813ae0a887a3725746e0ecf3b3798da6
EccentricVamp <EccentricVamp@gmail.com>
2021-06-19 20:25:09 +0000

    Wrap main with a function and convert to async.

--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,56 @@
 'use strict';
 
-var https = require('https');
 var path = require('path');
-var os = require('os');
 var fs = require('fs');
+var https = require('https');
+var os = require('os');
 
 const BRANCH_PREFIX = "refs/heads/";
 const BRANCH_DEFAULT = "default";
 const INPUT_PREFIX = "INPUT_";
-function input(inputName) {
+const DIRECTORY = "dist";
+async function run() {
+    let branch = getInput("branch");
+    if (branch === undefined || branch === "")
+        branch = BRANCH_DEFAULT;
+    else
+        branch = branch.replace(BRANCH_PREFIX, "");
+    process.stdout.write(`Branch: ${branch}${os.EOL}`);
+    const token = getInput("token");
+    if (token === undefined)
+        throw new Error("Missing input: token");
+    const modules = await getModules(DIRECTORY);
+    const code = {
+        branch: branch,
+        modules: Object.fromEntries(modules)
+    };
+    deploy(token, code);
+}
+function getInput(inputName) {
     const key = inputName
         .replace(/ /g, '_') // Replace spaces with underscores
         .toUpperCase();
     return process.env[INPUT_PREFIX + key];
 }
-let branch = input("branch");
-if (branch === undefined || branch === "")
-    branch = BRANCH_DEFAULT;
-else
-    branch = branch.replace(BRANCH_PREFIX, "");
-process.stdout.write(`Branch: ${branch}${os.EOL}`);
-const token = "b87011c5-a6aa-4625-ad00-84c3659b519c"; //input("token");
-const root = "dist";
-fs.opendir(root, (error, directory) => {
-    if (error !== null)
-        console.error(error);
-    else
-        getModules("", directory);
-});
-function getModules(prefix, directory) {
+async function getModules(path$1, prefix = "", modules = null) {
+    const directory = await fs.promises.opendir(path$1);
     process.stdout.write(`Directory: ${directory.path}${os.EOL}`);
-    const modules = new Map();
-    let entry;
-    while ((entry = directory.readSync()) !== null) {
+    modules !== null && modules !== void 0 ? modules : (modules = new Map());
+    for await (const entry of directory) {
         process.stdout.write(`Entry: ${entry.name}${os.EOL}`);
+        const entryPath = path.join(directory.path, entry.name);
         if (entry.isFile() && entry.name.endsWith(".js")) {
-            const entryPath = path.join(directory.path, entry.name);
             const module = prefix + entry.name.replace(/\.js$/i, "");
-            const contents = fs.readFileSync(entryPath, 'utf-8');
+            const contents = await fs.promises.readFile(entryPath, 'utf-8');
             modules.set(module, contents);
         }
+        else if (entry.isDirectory()) {
+            getModules(entryPath, prefix + entry.name + ".", modules);
+        }
     }
-    directory.closeSync();
-    deploy(modules);
+    return modules;
 }
-function deploy(modules) {
-    const data = {
-        branch: branch,
-        modules: Object.fromEntries(modules)
-    };
+function deploy(token, data) {
     const request = https.request({
         hostname: "screeps.com",
         port: 443,
@@ -66,3 +68,4 @@ function deploy(modules) {
     });
     request.end();
 }
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
+import { join } from "path";
+import { promises as fs, Dir } from "fs";
 import * as https from "https";
-import * as path from "path";
 import * as os from "os";
-import {
-  Dir as Directory,
-  opendir as openDirectory,
-  readFileSync
-} from "fs";
 
 const BRANCH_PREFIX = "refs/heads/";
 const BRANCH_DEFAULT = "default";
 const INPUT_PREFIX = "INPUT_";
+const DIRECTORY = "dist";
 
-function input(inputName: string): string | undefined {
+async function run(): Promise<void> {
+  let branch = getInput("branch");
+  if (branch === undefined || branch === "") branch = BRANCH_DEFAULT;
+  else branch = branch.replace(BRANCH_PREFIX, "");
+  process.stdout.write(`Branch: ${branch}${os.EOL}`);
+
+  const token = getInput("token");
+  if (token === undefined) throw new Error ("Missing input: token");
+
+  const modules = await getModules(DIRECTORY);
+  const code = {
+    branch: branch,
+    modules: Object.fromEntries(modules)
+  }
+
+  deploy(token, code);
+}
+
+function getInput(inputName: string): string | undefined {
   const key = inputName
     .replace(/ /g, '_') // Replace spaces with underscores
     .toUpperCase();
@@ -19,49 +34,34 @@ function input(inputName: string): string | undefined {
   return process.env[INPUT_PREFIX + key];
 }
 
-let branch = input("branch");
-if (branch === undefined || branch === "") branch = BRANCH_DEFAULT;
-else branch = branch.replace(BRANCH_PREFIX, "");
-process.stdout.write(`Branch: ${branch}${os.EOL}`);
+async function getModules(
+  path: string,
+  prefix: string = "",
+  modules: Map<string, string> | null = null
+): Promise<Map<string, string>> {
+  const directory = await fs.opendir(path);
+  process.stdout.write(`Directory: ${directory.path}${os.EOL}`);
+  modules ??= new Map<string, string>();
 
-const token = "b87011c5-a6aa-4625-ad00-84c3659b519c"; //input("token");
-if (token === undefined) throw new Error ("Missing input: token");
-
-const root = "dist";
-openDirectory(root, (error, directory) => {
-  if (error !== null) console.error(error);
-  else getModules("", directory);
-});
-
-function getModules(
-  prefix: string,
-  directory: Directory
-) {
-  process.stdout.write(`Directory: ${directory.path}${os.EOL}`)
-  const modules = new Map<string, string>();
-
-  let entry;
-  while ((entry = directory.readSync()) !== null) {
+  for await (const entry of directory) {
     process.stdout.write(`Entry: ${entry.name}${os.EOL}`)
+    const entryPath = join(directory.path, entry.name);
 
     if (entry.isFile() && entry.name.endsWith(".js")) {
-      const entryPath = path.join(directory.path, entry.name);
       const module = prefix + entry.name.replace(/\.js$/i, "");
-      
-      const contents = readFileSync(entryPath, 'utf-8');
+      const contents = await fs.readFile(entryPath, 'utf-8');
       modules.set(module, contents);
+    } else if (entry.isDirectory()) {
+      getModules(entryPath, prefix + entry.name + ".", modules);
     }
   }
-  directory.closeSync();
-  deploy(modules);
+  return modules;
 }
 
-function deploy(modules: Map<string, string>) {
-  const data = {
-    branch: branch,
-    modules: Object.fromEntries(modules)
-  };
-
+function deploy(
+  token: string,
+  data: any,
+): void {
   const request = https.request({
     hostname: "screeps.com",
     port: 443,
@@ -80,3 +80,4 @@ function deploy(modules: Map<string, string>) {
   request.end();
 }
 
+run();
